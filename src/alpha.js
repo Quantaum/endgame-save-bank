5016aa14fb9b2a26578a211cb976ae2874e560fd
Quantaum <crescentiumlmao@gmail.com>
2026-02-04 20:03:09 -0800

    asdsad

--- src/alpha.js
+++ src/alpha.js
@@ -1,9 +1,9 @@
 /* eslint-disable max-len */
 import CategoryGrouped from "./classes/CategoryGrouped";
-import alphaPreInfinity from "./alpha-pre-infinity";
-import alphaPreEternity from "./alpha-pre-eternity";
-import alphaPreDilation from "./alpha-pre-dilation";
-import alphaPreReality from "./alpha-pre-reality";
+import { alphaPreInfinity } from "./alpha-pre-infinity";
+import { alphaPreEternity } from "./alpha-pre-eternity";
+import { alphaPreDilation } from "./alpha-pre-dilation";
+import { alphaPreReality } from "./alpha-pre-reality";
 
 export const alpha = new CategoryGrouped({
     name: "Alpha",
