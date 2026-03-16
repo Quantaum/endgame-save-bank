cf2fcab742caaf3090d66ccc2c8e1142f07dc09c
Quantaum <crescentiumlmao@gmail.com>
2026-03-15 18:32:27 -0700

    asdasdasdasd

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import { endgame } from "./endgame";
 import { alpha } from "./alpha";
-/*import { celDimExpansion } from "./cdexpansion"
+import { celDimExpansion } from "./cdexpansion";
 import { slabdrill } from "./slabdrill";
 import { expanse } from "./expanse";
 import { existence } from "./existence";
-import { shatteredReality } from "./shattered-reality";*/
+import { shatteredReality } from "./shattered-reality";
 
 import CategoryGrouped from "./classes/CategoryGrouped";
 import File from "./classes/File";
@@ -12,11 +12,11 @@ import File from "./classes/File";
 const saves = [
     endgame,
     alpha,
-    /*celDimExpansion,
+    celDimExpansion,
     slabdrill,
     expanse,
     existence,
-    shatteredReality*/
+    shatteredReality
 ];
 
 const downloadAllSaves = File.downloadAllSaves;
